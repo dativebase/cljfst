92b5dfd8917d12b7edbeba225538bb506b0554d7
Joel Dunham <jrwdunham@gmail.com>
2016-12-26 20:48:20 -0800

    Implemented Hopcroft canonical minimization algorithm

--- src/cljfst/core.clj
+++ src/cljfst/core.clj
@@ -342,7 +342,6 @@
   [cont-vec [fst N]]
   (into cont-vec [[fst N]]))
 
-
 (defn merge-alphabets
   "Merge the alphabets of two fsts. Takes two fsts and returns them, with
   modified alphabets."
@@ -354,11 +353,14 @@
       []
       [[fst1 N-1] [fst2 N-2]])))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Hopcroft Canonical Minimization Functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn incoming-transition
   "Return true if you can use `symbol` to transition into one of the states in
   `states` using `transition`."
   [transition symbol states]
-  (println "DEBUG incoming transition")
   (if (and (= symbol (second transition))
            (some #{(nth transition 2)} states))
     true
@@ -368,14 +370,11 @@
   "Return whichever of final-states or non-final-states has the fewest incoming
   transitions to symbol, given delta."
   [delta symbol final-states non-final-states]
-  (println "DEBUG fewest-incoming-transitions")
   (let [into-final (filter
                      (fn [x] (incoming-transition x symbol final-states)) delta)
         into-non-final (filter
                          (fn [x] (incoming-transition x symbol non-final-states))
                          delta)]
-    (println "into-final")
-    (println into-final)
     (if (< (count into-final) (count into-non-final))
       final-states
       non-final-states)))
@@ -385,7 +384,6 @@
   final-states or non-final-states, depending on which has fewer incoming
   transitions for that symbol"
   [final-states non-final-states sigma delta]
-  (println "DEBUG get-hcc-agenda")
   (reduce
     (fn [container symbol]
       (conj container
@@ -395,24 +393,229 @@
     []
     sigma))
 
-(defn hopcroft-canonical-minimization
-  "Perform Hopcroft canonical minimization on `fst`, cf. Hulden p. 80"
+(defn count-incoming-transitions-from-state
+  [fst symbol state]
+  (reduce
+    +
+    (count (filter
+      (fn [transition]
+        (and (= symbol (second transition))
+           (= state (nth transition 2))))
+      (:delta fst)))))
+
+(defn count-incoming-transitions-from-states
+  "Return the number of incoming transitions into all states in `states` using
+  where `symbol` ."
+  [fst symbol states]
+  (reduce
+    +
+    (map
+      (fn [state]
+          (count-incoming-transitions-from-state fst symbol state))
+      states)))
+
+(defn get-incoming-transitions
+  "Return incoming transitions for a Hopcroft canonical agenda item, i.e., a
+  block (vector) of states and a symbol; that is, return a mapping from each
+  state `s` to all transitions such that one of the states in `block` is
+  accessible from state `s` by reading in symbol `symbol`."
+  [block symbol fst]
+  (into
+    {}
+    (map
+      (fn [state]
+        [state
+          (filter
+            (fn [transition]
+              (let [pred (and (= (first transition) state)
+                              (= (second transition) symbol)
+                              (some #{(nth transition 2)} block))]
+                pred))
+            (:delta fst))])
+      (:Q fst))))
+
+(defn get-new-Pi-and-splits
+  "Returns a hash mapping `:new-Pi` to a vector of vectors of states (a
+  new partition of the states in an FST) and `:splits` to a vector of
+  vectors, each containing a partition (of states) as first element and a 2-ary
+  sub-partition of that partition, the first being those states that are
+  'incoming' given `incoming-transition` and the second being those that are
+  not."
+  [Pi incoming-transitions]
+  (let [new-Pi-and-splits
+        (reduce
+          (fn [container partition]
+            ;; `partition` is a vector of states
+            (if (> (count partition) 1)
+              ;; split partition into those in incoming-transitions and those not.
+              (let [{subpart-incoming true subpart-non-incoming false}
+                    (group-by
+                      (fn [x] (> (count (get incoming-transitions x)) 0))
+                      partition)]
+                (assoc
+                  (assoc container :new-Pi
+                        (conj (:new-Pi container) subpart-incoming
+                              subpart-non-incoming))
+                  :splits
+                  (conj (:splits container) [partition [subpart-incoming
+                                                        subpart-non-incoming]])))
+              ;; elsewhere case: partition is un-split-able
+              (assoc container :new-Pi (conj (:new-Pi container) partition))))
+          {:new-Pi [] :splits []}
+          Pi)]
+    new-Pi-and-splits))
+
+(defn get-agenda-item-size
+  [[states symbol] delta]
+  (reduce + (map
+              (fn [state]
+                (count
+                  (filter
+                    (fn [transition]
+                      (and
+                        (= symbol (second transition))
+                        (= state (nth transition 2))))
+                    delta)))
+              states)))
+
+(defn get-hcc-min
+  [cand1 cand2 delta]
+  (let [cand1-size
+        (get-agenda-item-size cand1 delta)
+        cand2-size
+        (get-agenda-item-size cand2 delta)]
+    (if (< cand1-size cand2-size) cand1 cand2)))
+
+(defn cart [colls]
+  (if (empty? colls)
+    '(())
+    (for [x (first colls)
+          more (cart (rest colls))]
+      (cons x more))))
+
+(defn get-agenda-mods
+  "Return a hash map of agenda modifications (for the Hopcroft canonical
+  minimization algorithm) based on the vector of new splits."
+  [new-splits fst hcc-agenda]
+  (let [result
+    (reduce
+      (fn [container [split symbol]]
+        (let [[partition [subpart-incoming subpart-non-incoming]] split]
+          ;; if the partition-symbol pair is on agenda...
+          (if (some #{[partition symbol]} hcc-agenda)
+            ;; ... add to :replace-on-agenda a 2-ary vector whose first element
+            ;; is the to-be-replaced agenda item and whose second element is a
+            ;; vector of replacement agenda items.
+            (assoc container :replace-on-agenda
+                  (conj (:replace-on-agenda container)
+                        [[partition symbol] [[subpart-incoming symbol]
+                                              [subpart-non-incoming symbol]]]))
+            ;; otherwise, add to agend the "Min" of `[subpart-incoming symbol]`
+            ;; and `[subpart-non-incoming symbol]`, where "Min" means ...
+            (let [min (get-hcc-min [subpart-incoming symbol]
+                                   [subpart-non-incoming symbol]
+                                   (:delta fst))]
+              (assoc container :add-to-agenda (conj (:add-to-agenda container)
+                                                    min))))))
+    {:replace-on-agenda [] :add-to-agenda []}
+    (cart [new-splits (:sigma fst)]))]
+    result))
+
+(defn get-new-hcc-agenda
+  [hcc-agenda agenda-mods]
+  (reduce
+    (fn [new-agenda existing-agenda-item]
+      (let [replacement
+            (first (filter (fn [mapping] (= (first mapping)
+                                            existing-agenda-item))
+                           (:replace-on-agenda agenda-mods)))]
+        (if replacement
+          (concat new-agenda replacement)
+          (conj new-agenda existing-agenda-item))))
+    (:add-to-agenda agenda-mods)
+    hcc-agenda))
+
+(defn agenda-to-equiv-classes
+  "Takes a Hopcroft canonical agenda (a vector containing 2-vectors, which each
+  contain a vector of states and a symbol from the alphabet) and Pi, a
+  partitioning of the states in a given FST, and returns Pi as a set of
+  equivalence classes of states, i.e., states that are equivalent and can be
+  merged."
+  [fst hcc-agenda Pi]
+  (let [agenda-item (first hcc-agenda)]
+    (if agenda-item
+      ;; examine the first agenda item
+      (let [[block symbol] agenda-item
+            ;; maps states to vectors of their incoming transitions to any
+            ;; state in `block` via `symbol`
+            incoming-transitions (get-incoming-transitions block symbol fst)]
+        (if (> (reduce + (map count (vals incoming-transitions))) 0)
+          (let [new-Pi-and-splits
+                (get-new-Pi-and-splits Pi incoming-transitions)
+                agenda-mods
+                (get-agenda-mods (:splits new-Pi-and-splits) fst hcc-agenda)
+                new-hcc-agenda
+                (get-new-hcc-agenda (rest hcc-agenda) agenda-mods)]
+            (recur fst new-hcc-agenda (:new-Pi new-Pi-and-splits)))
+            (recur fst (rest hcc-agenda) Pi)))
+      Pi)))
+
+(defn hopcroft-canonical-equiv-classes
+  "Perform Hopcroft canonical minimization on `fst`, cf. Hulden p. 80. Takes an
+  FST as input and returns Pi, a set of equivalence classes, i.e., sets of
+  states that are equivalent and can be merged."
   [fst]
-  (println "DEBUG hopcroft")
   (let [final-states (:F fst)
         non-final-states (into []
                                (clojure.set/difference
                                  (set (:Q fst))
                                  (set final-states)))
         Pi [final-states non-final-states]
-        hcc-agenda (get-hcc-agenda final-states non-final-states (:sigma fst)
-                                   (:delta fst))]
-    (println "final-states:")
-    (println final-states)
-    (println "non-final-states:")
-    (println non-final-states)
-    (println "returning hcc-agenda:")
-    hcc-agenda))
+        hcc-agenda
+        (get-hcc-agenda final-states non-final-states (:sigma fst) (:delta
+                                                                     fst))]
+    (agenda-to-equiv-classes fst hcc-agenda Pi)))
+
+(defn get-minimize-fixer
+  [equiv-classes Q]
+  (let [fixers (filter #(> (count %) 1) equiv-classes)]
+    (reduce
+      (fn [container state]
+        (if (= state :s0)
+          (assoc container state state)
+          (let [fixer (first (filter #(some #{state} %) fixers))]
+            (if fixer
+              (assoc container state (first fixer))
+              (assoc container state state)))))
+      {}
+      Q)))
+
+(defn minimize-state-set
+  [state-set fixer]
+  (into [] (set (map (fn [state] (get fixer state state)) state-set))))
+
+(defn minimize-delta
+  [delta fixer]
+  (into [] (set (map (fn [[st-i sy-i st-o sy-o]]
+         [(get fixer st-i st-i)
+          sy-i
+          (get fixer st-o st-o)
+          sy-o])
+       delta))))
+
+(defn minimize-hcc
+  [fst]
+  (let [equiv-classes (hopcroft-canonical-equiv-classes fst)
+        fixer (get-minimize-fixer equiv-classes (:Q fst))]
+    {:sigma (:sigma fst)
+     :Q (minimize-state-set (:Q fst) fixer)
+     :s0 :s0
+     :F (minimize-state-set (:F fst) fixer)
+     :delta (minimize-delta (:delta fst) fixer)}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; END Hopcroft Canonical Minimization Functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn -main
   "Provide an AT&T-formatted FST path and an input string and behold the
--- test/cljfst/core_test.clj
+++ test/cljfst/core_test.clj
@@ -162,6 +162,7 @@
       (is (= "ab" (first (apply-down fst "ab"))))
       (is (empty? (apply-down fst "aba"))))))
 
+;; From Hulden p. 82
 (def non-minimized-fst
   {:sigma ["a" "b"],
    :Q [:s0 :s1 :s2 :s3 :s4],
@@ -178,6 +179,28 @@
            [:s4 "b" :s2 "b"]
            [:s4 "a" :s1 "a"]]})
 
-(println "Hopcroft Agenda:")
-(println (hopcroft-canonical-minimization non-minimized-fst))
+(deftest test-hopcroft-canonical-equiv-classes
+  (testing "Hopcroft canonical minimization produces correct equivalence
+           classes"
+    (let [equiv-classes (hopcroft-canonical-equiv-classes non-minimized-fst)]
+      (is (= #{[:s4] [:s3] [:s1] [:s0 :s2]} (set equiv-classes))))))
 
+(clojure.pprint/pprint (minimize-hcc non-minimized-fst))
+
+;; When minimized, all instances of :s2 in `non-minimized-fst` should be
+;; replaced with :s0
+(deftest test-hopcroft-canonical-minimization
+  (testing "Hopcroft canonical minimization algorithm works"
+    (let [minimized-fst (minimize-hcc non-minimized-fst)
+          expected-delta (set [
+                              [:s0 "a" :s1 "a"]
+                              [:s0 "b" :s0 "b"]
+                              [:s1 "a" :s1 "a"]
+                              [:s1 "b" :s3 "b"]
+                              [:s3 "a" :s1 "a"]
+                              [:s3 "b" :s4 "b"]
+                              [:s4 "b" :s0 "b"]
+                              [:s4 "a" :s1 "a"]])]
+      (is (= #{:s0 :s1 :s3 :s4} (set (:Q minimized-fst))))
+      (is (= [:s4] (:F minimized-fst)))
+      (is (= expected-delta (set (:delta minimized-fst)))))))
