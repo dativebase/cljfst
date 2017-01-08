453583202407d2ca3bbc6d815a0296616aae733f
Joel Dunham <jrwdunham@gmail.com>
2017-01-08 09:08:50 -0800

    Fixed apply-down bug

--- src/cljfst/apply.clj
+++ src/cljfst/apply.clj
@@ -13,15 +13,15 @@
         (string/blank? input)))
 
 (defn get-transitions
-  "Return a seq of out-state/out-symbol pairs for the given in-state/in-symbol
-  pair in the given fst"
-  [fst st-i input]
+  "Return the transitions that allow the reading of some prefix of string
+  `input` when in state `st-i`."
+  [fst st-i input sigma]
   (filter
     (fn [[tr-st-i tr-sy-i _ __]]
       (and (= st-i tr-st-i)
            (or (= epsilon-symbol tr-sy-i)
                (= input tr-sy-i)
-               (= unknown-symbol tr-sy-i)
+               (and (= unknown-symbol tr-sy-i) (not (some #{(str (first input))} sigma)))
                (= identity-symbol tr-sy-i)
                (string/starts-with? input tr-sy-i))))
     (:delta fst)))
@@ -37,16 +37,27 @@
           (= identity-symbol sy-i) (str (first input))
           (= unknown-symbol sy-i) (str (first input))
           :else sy-i)
+        ;; TODO: here you shouldn't be able to read unknown-symbol if (first input) is in alphabet
+        ;; debug (println "********************************************************************************")
+        ;; debug (println (str "consumed \"" consumed "\""))
         new-input (string/replace-first input consumed "")
+        ;; debug (println (str "input changed from \"" input "\" to \"" new-input "\""))
         new-outputs
         (map
           (fn [output]
-            (cond
-              (= epsilon-symbol sy-o) output
-              (= identity-symbol sy-o) (str output consumed)
-              (= unknown-symbol sy-o) (str output "?")
-              :else (str output sy-o)))
-          outputs)]
+            ;; (println (str "changing output \"" output "\" to ..."))
+            (let [result
+                  (cond
+                    (= epsilon-symbol sy-o) output
+                    (= identity-symbol sy-o) (str output consumed)
+                    (= unknown-symbol sy-o) (str output "?")
+                    :else (str output sy-o))]
+              ;; (println result)
+              result
+              ))
+          outputs)
+          ;; debug (println "********************************************************************************")
+          ]
     [new-input new-outputs]))
 
 ;; TODO: apply-down should first ascertain whether the fst is cyclic. If it is,
@@ -57,14 +68,16 @@
   ([fst input] (apply-down fst input (:s0 fst) [""]))
   ([fst input state outputs]
    (if (in-final-with-empty-input input state fst)
-     outputs
-     (let [transitions (get-transitions fst state input)]
-       (reduce
+     (set outputs)
+     (let [transitions (get-transitions fst state input (:sigma fst))]
+       (set (reduce
          concat
          []
          (map
            (fn [[_ sy-i next-state sy-o]]
              (let [[new-input new-outputs]
-                   (consume-input input sy-i sy-o outputs)]
+                   (consume-input input sy-i sy-o outputs)
+                   ;; debug (println (str "in state " state " with outputs " (pr-str outputs) ", reading input " input " and moving to state " next-state " with new input " new-input " and new outputs " (pr-str new-outputs)))
+                   ]
                (apply-down fst new-input next-state new-outputs)))
-           transitions))))))
+           transitions)))))))
